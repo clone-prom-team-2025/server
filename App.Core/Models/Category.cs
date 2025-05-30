8c9586a152c2d2e053c908d7afce8c23cb0a9526
pashhun <pavlikvstaras@gmail.com>
2025-05-30 21:52:27 +0300

    add-category-model

--- App.Core/Models/Category.cs
+++ App.Core/Models/Category.cs
@@ -1,5 +1,6 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Core.Models;
 
@@ -7,4 +8,10 @@ public class Category
 {
     [BsonId]
     public ObjectId Id { get; set; }
+    
+    [StringLength(100, MinimumLength = 3)]
+    public string Name { get; set; }
+    
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? ParentId { get; set; }
 }
